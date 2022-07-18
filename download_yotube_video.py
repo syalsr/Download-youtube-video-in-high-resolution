3e817c60cf0047cfcb1a0a6e5345b60e370fa042
volobuevrostislav <emil07102000@gmail.com>
2022-07-18 23:14:30 +0300

    fix name output video

--- download_yotube_video.py
+++ download_yotube_video.py
@@ -4,31 +4,32 @@ import ffmpeg
 
 path = './'
 count = 1
-result_name = ''
 
-def combine(video, audio):
+def combine(video, audio, name):
     video_stream = ffmpeg.input(video)
     audio_stream = ffmpeg.input(audio)
-    ff = ffmpeg.output(audio_stream, video_stream, path + "result_name.mp4").run()
+    
+    ff = ffmpeg.output(audio_stream, video_stream, path + name + '.mp4').run()
 
 def download_video(link):
     yt = YouTube(link)
     video = yt.streams.filter(file_extension='mp4').order_by('resolution').last()
     video_name = 'video' + video.title + '.mp4'
     result_name = video.title
+    print('RESULT NAME: ', result_name)
     video.download(path, video_name)
     if not video.is_progressive:
         audio = yt.streams.get_audio_only()
         audio_name = 'audio' + audio.title + 'mp4'
         audio.download(path, audio_name)
-        combine(path + video_name, path + audio_name)
+        combine(path + video_name, path + audio_name, video.title)
     os.remove(audio_name)
     os.remove(video_name)
 
 def parse_links():
     list_links = list()
     while True:
-        inp = input("Enter link or exit for: ")
+        inp = input("Enter link or exit to start downloading: ")
         if inp == "exit":
             break
         list_links.append(inp)
